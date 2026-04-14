5eaeb701f4bc0d53c02dec0824dbe6c7c038c7ef
Norman Yee <155019+funkadelic@users.noreply.github.com>
2026-04-13 19:57:51 -0700

    ci: add release-please version annotation to const.py

--- custom_components/rainpoint/const.py
+++ custom_components/rainpoint/const.py
@@ -2,7 +2,7 @@
 DOMAIN = "rainpoint"
 
 # Integration version
-VERSION = "1.0.0"
+VERSION = "1.0.0"  # x-release-please-version
 
 # Issue tracker URL
 ISSUE_URL = "https://github.com/funkadelic/ha-rainpoint/issues"
